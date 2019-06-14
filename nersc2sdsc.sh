db03bd7eed9261b1b37f74c79d144b226b4d5496
Erfan Nourbakhsh <erfanxyz@gmail.com>
2019-06-14 01:43:54 -0700

    Update nersc2sdsc.sh

--- nersc2sdsc.sh
+++ nersc2sdsc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+script screen.txt
 start_time="$(date -u +%s)"
 
 # --------------------------
@@ -21,6 +22,8 @@ do
     fi
 done
 
+exit # exits the screen session and saves the logs to `screen.log`
+
 end_time="$(date -u +%s)"
 secs="$(($end_time-$start_time))"
 
@@ -35,8 +38,12 @@ body+="Hi Tony and Erfan,\n\n"
 body+=$(printf "Congratulations! Your file transfer from NERSC to SDSC has completed successfully in %02d:%02d:%02d hms." $(($secs/3600)) $(($secs%3600/60)) $(($secs%60)))
 body+="\n\nSent by Erfan's automated script on GitHub :)"
 
-echo -e "$body" | mail -s "$subject" "erfan@ucdavis.edu"
+echo -e "$body" | mailx -s "$subject" -a screen.txt "erfan@ucdavis.edu"
 
 # used ref:
 # https://unix.stackexchange.com/questions/48298/can-rsync-resume-after-being-interrupted
 # https://stackoverflow.com/questions/8260858/how-to-send-email-from-terminal
+# echo 'mail content' | mailx -s 'email subject' -a attachment.txt username@stackoverflow.com
+
+# usage:
+# bash <(curl -s https://github.com/cosmicshear/BlendSim/blob/master/nersc2sdsc.sh)
