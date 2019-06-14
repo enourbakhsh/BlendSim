a14d760b229d89b49f9ff604c9eea4b6d479d796
Erfan Nourbakhsh <erfanxyz@gmail.com>
2019-06-14 01:59:24 -0700

    Update nersc2sdsc.sh

--- nersc2sdsc.sh
+++ nersc2sdsc.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# script screen.txt
 start_time="$(date -u +%s)"
 
 # --------------------------
 # - file transfer settings
 # --------------------------
 
+logfile=screen.txt
 source=erfan@emerald.physics.ucdavis.edu:/nfs/home/erfan/airflow/logs/*.logs
 dest=/oasis/projects/nsf/ddp340/erfan/rsync_test/
 
 while [ 1 ]
 do
-    rsync -avz --partial $source $dest
+    rsync -avz --partial $source $dest --log-file=$logfile
     if [ "$?" = "0" ] ; then
         echo "rsync completed successfully!"
         break
@@ -21,8 +21,6 @@ do
         sleep 180
     fi
 done
-
-#exit # exits the screen session and saves the logs to `screen.log`
 
 end_time="$(date -u +%s)"
 secs="$(($end_time-$start_time))"
@@ -38,7 +36,7 @@ body+="Hi Tony and Erfan,\n\n"
 body+=$(printf "Congratulations! Your file transfer from NERSC to SDSC has completed successfully in %02d:%02d:%02d hms." $(($secs/3600)) $(($secs%3600/60)) $(($secs%60)))
 body+="\n\nSent by Erfan's automated script on GitHub :)"
 
-echo -e "$body" | mailx -s "$subject" -a screen.txt "erfan@ucdavis.edu"
+echo -e "$body" | mailx -s "$subject" -a "$logfile" "erfan@ucdavis.edu"
 
 # used ref:
 # https://unix.stackexchange.com/questions/48298/can-rsync-resume-after-being-interrupted
@@ -46,8 +44,6 @@ echo -e "$body" | mailx -s "$subject" -a screen.txt "erfan@ucdavis.edu"
 # echo 'mail content' | mailx -s 'email subject' -a attachment.txt username@stackoverflow.com
 
 # usage:
-# bash <(curl -s https://github.com/cosmicshear/BlendSim/raw/master/nersc2sdsc.sh)
-# curl -sSL https://github.com/cosmicshear/BlendSim/raw/master/nersc2sdsc.sh | sh
-# bash <(wget -qO- https://github.com/cosmicshear/BlendSim/raw/master/nersc2sdsc.sh)
+# bash <(wget --delete-after -qO- https://github.com/cosmicshear/BlendSim/raw/master/nersc2sdsc.sh)
 
 
